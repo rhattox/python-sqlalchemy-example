2005843c8cfbc797fbb18ac129e481c1398f4710
Bruno Souto <bcovies@gmail.com>
2023-11-27 23:04:12 -0300

    Add new PSQL connection and DATABASE Creation

--- src/main.py
+++ src/main.py
@@ -1,16 +1,38 @@
-import sqlalchemy
-# import psycopg2
-# print(psycopg2.__version__)
-# print(sqlalchemy.__version__  )
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 from sqlalchemy import create_engine
 
 # default
-engine = create_engine("postgresql://postgres:postgres@db:5432/python_database")
-print(engine)
-from sqlalchemy import text
+engine = create_engine("postgresql://postgres:postgres@db:5432/python_database", echo=True)
 
-with engine.connect() as connection:
-    result = connection.execute(text("select username from users"))
-    for row in result:
-        print("username:", row.username)
+Base.metadata.create_all(engine)
