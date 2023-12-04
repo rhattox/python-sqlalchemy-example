c280ecea7566a06e6da7a08ef11982c7b8255e68
Bruno Souto <bcovies@gmail.com>
2023-12-04 16:37:33 -0300

    Add new content

--- src/main.py
+++ src/main.py
@@ -38,3 +38,32 @@ engine = create_engine("postgresql://postgres:postgres@db:5432/python_database",
 
 Base.metadata.create_all(engine)
 
+from sqlalchemy.orm import Session
+
+
+with Session(engine) as session:
+    spongebob = User(
+        name="spongebob",
+        fullname="Spongebob Squarepants",
+        addresses=[Address(email_address="spongebob@sqlalchemy.org")],
+    )
+    sandy = User(
+        name="sandy",
+        fullname="Sandy Cheeks",
+        addresses=[
+            Address(email_address="sandy@sqlalchemy.org"),
+            Address(email_address="sandy@squirrelpower.org"),
+        ],
+    )
+    patrick = User(name="patrick", fullname="Patrick Star")
+    session.add_all([spongebob, sandy, patrick])
+    session.commit()
+
+from sqlalchemy import select
+
+session = Session(engine)
+
+stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
+
+for user in session.scalars(stmt):
+    print(user)
